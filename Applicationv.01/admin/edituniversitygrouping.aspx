c2388c431a0a697e8342097fb6c268950b58a35b
Akash <akash@sodelsolutions.com>
2019-06-12 12:46:34 +0530

    Added adduniversitygrouping, universitygroupingmater and edituniversitygrouping

---
+++ Applicationv.01/admin/edituniversitygrouping.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="edituniversitygrouping.aspx.cs" Inherits="admin_edituniversitygrouping" MasterPageFile="~/admin/admin.master" %>
+
+<asp:Content ID="content1" runat="server" ContentPlaceHolderID="head">
+</asp:Content>
+<asp:Content ID="content2" runat="server" ContentPlaceHolderID="ContentPlaceHolder1">
+    <div class="container page__container">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="Default.aspx">Home</a></li>
+            <li class="breadcrumb-item active">Edit University Grouping</li>
+        </ol>
+
+        <h1 class="h2">Edit University Grouping</h1>
+
+        <div class="card">
+            <div class="tab-content card-body">
+                <div class="tab-pane active" id="first">
+                    
+                    <div class="form-group row">
+                         <label for="groupheadUniversity" class="col-sm-3 col-form-label form-label">Group Head University</label>
+                            <div class="col-sm-8">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <asp:DropDownList ID="ddlUniversity" runat="server" class="form-control"></asp:DropDownList>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-8">
+                        <div class="row">
+                            <asp:CheckBoxList ID="checkBoxList" runat="server"></asp:CheckBoxList>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-sm-8 offset-sm-3">
+                            <div class="media align-items-center">
+                                <div class="media-left">
+                                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="btn btn-primary btn-block" OnClick="btnSubmit_Click" OnClientClick="return validateForm()"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#<%=ddlUniversity.ClientID%>").change(function () {
+                $("#<%=checkBoxList.ClientID%>").empty();
+                var universityValue = $('#<%=ddlUniversity.ClientID%>').val();
+                $('#<%=ddlUniversity.ClientID%> option').each(function (index, option) {
+                    if (index != 0 && option.attributes.value.value != universityValue) {
+                        var id = $(this).val();
+                        var value = $(this).val();
+                        var text = option.innerText;
+                        $('<label />', { 'for': 'cb' + id, id:'label'+id }).appendTo("#<%=checkBoxList.ClientID%>");
+                        $('<input />', { type: 'checkbox', id: 'cb' + id, value: value, name: 'university' }).appendTo('#label' + id);
+                        $('#label' + id).append(text);
+                    }
+                });
+            });
+
+        });
+        function validateForm() {
+            var universityValue = $('#<%=ddlUniversity.ClientID%>').val();
+            var count = 0;
+            $('#<%=checkBoxList.ClientID%> input:checked').each(function () {
+                count++;
+            });
+
+            if (universityValue == 0 || isNaN(parseInt(universityValue))) {
+                alert("Please select University");
+                return false;
+            }
+            else if (count == 0) {
+                alert("Please check university for mapping");
+                return false;
+            }
+
+            return true;
+            
+        }
+    </script>
+
+</asp:Content>
---
+++ Applicationv.01/admin/edituniversitygrouping.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class admin_edituniversitygrouping : System.Web.UI.Page
+{
+    private GTEEntities db = new GTEEntities();
+
+    Logger objLog = new Logger();
+    string webURL = System.Configuration.ConfigurationManager.AppSettings["WebUrl"].ToString();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["headUniversityID"] != null)
+        {
+            int headUniversityId;
+            if (!Int32.TryParse(Request.QueryString["headUniversityID"], out headUniversityId))
+                Response.Redirect("~/admin/default.aspx");
+
+            if (!IsPostBack)
+            {
+                var existingMapping = (from mappings in db.universitygrouping
+                                       where mappings.groupingheaduniversityid == headUniversityId
+                                       select mappings).ToList();
+
+                if (existingMapping != null)
+                {
+                    ViewState["headUniversityID"] = headUniversityId;
+                    bindUniversityDropDown(headUniversityId);
+
+                    List<university_master> notSelectedUniversityList = db.university_master.ToList();
+                    foreach (var item in existingMapping)
+                        notSelectedUniversityList.Remove(notSelectedUniversityList.Where(x => x.universityid == item.universityid).First());
+
+                    notSelectedUniversityList.Remove(notSelectedUniversityList.Where(x => x.universityid == headUniversityId).First());
+
+                    var selectedUniversityList = (from universities in db.university_master
+                                                  join mappings in db.universitygrouping on universities.universityid equals mappings.universityid
+                                                  where universities.universityid == mappings.universityid && mappings.groupingheaduniversityid == headUniversityId
+                                                  select universities).ToList();
+
+                    foreach (var item in notSelectedUniversityList)
+                    {
+                        if (item.universityid != headUniversityId)
+                        {
+                            ListItem checkBox = new ListItem();
+                            checkBox.Text = item.university_name;
+                            checkBox.Selected = false;
+                            checkBox.Value = Convert.ToString(item.universityid);
+                            checkBoxList.Items.Add(checkBox);
+                        }
+                    }
+
+                    foreach (var item in selectedUniversityList)
+                    {
+                        ListItem checkBox = new ListItem();
+                        checkBox.Text = item.university_name;
+                        checkBox.Selected = true;
+                        checkBox.Value = Convert.ToString(item.universityid);
+                        checkBoxList.Items.Add(checkBox);
+                        //set checked properties of checke box with mappings;
+                    }
+
+                }
+                else
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('No University/universities present under this university')", true);
+            }
+            
+        }
+        else
+            Response.Redirect("~/admin/default.aspx");
+    }
+
+    private void bindUniversityDropDown(int universityId)
+    {
+        try
+        {
+            ListItem lst = new ListItem("Please select university", "0");
+            List<university_master> universityMaster = db.university_master.ToList();
+
+            ddlUniversity.DataSource = universityMaster;
+            ddlUniversity.DataTextField = "university_name";
+            ddlUniversity.DataValueField = "universityid";
+            ddlUniversity.DataBind();
+            ddlUniversity.Items.Insert(0, lst);
+            ddlUniversity.SelectedIndex = universityId;
+        }
+        catch (Exception ex)
+        {
+            objLog.WriteLog(ex.StackTrace.ToString());
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        int groupHeadUniversity = Convert.ToInt32(ddlUniversity.SelectedItem.Value);
+        string[] selectedUniversity;
+
+        if (Request.Form["university"] == null)
+        {
+            selectedUniversity = new string[checkBoxList.Items.Count];
+            for (int i = 0, j = 0; i < checkBoxList.Items.Count; i++)
+            {
+                if (checkBoxList.Items[i].Selected == true) 
+                {
+                    selectedUniversity.SetValue(checkBoxList.Items[i].Value, j); // add selected Item text to the String.
+                    j++;
+                }
+            }
+        }
+        else
+            selectedUniversity = Request.Form["university"].Split(',');
+
+        int existingGroupHeadID = Convert.ToInt32(ViewState["headUniversityID"]);
+        var existingGrouping = (from mapping in db.universitygrouping
+                                where mapping.groupingheaduniversityid == existingGroupHeadID
+                                select mapping).ToList();
+
+        db.universitygrouping.RemoveRange(existingGrouping);
+        db.SaveChanges();
+        try
+        {
+            foreach (string s in selectedUniversity)
+            {
+                if (!string.IsNullOrEmpty(s))
+                {
+                    universitygrouping groupingObj = new universitygrouping();
+                    groupingObj.groupingheaduniversityid = groupHeadUniversity;
+                    groupingObj.universityid = Convert.ToInt32(s);
+
+                    db.universitygrouping.Add(groupingObj);
+                    db.SaveChanges();
+                }
+            }
+            Response.Redirect("~/admin/universitygroupingmaster.aspx");
+        }
+        catch (Exception ex)
+        {
+            objLog.WriteLog(ex.StackTrace.ToString());
+        }
+    }
+}
