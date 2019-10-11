014b104cdf92e666a91fc53b17e6cbe7bc87932e
Aishwarya <aishwarya@sodelsolutions.com>
2019-10-11 12:18:14 +0530

    change in editcourse page

--- Applicationv.01/admin/editcourse.aspx.cs
+++ Applicationv.01/admin/editcourse.aspx.cs
@@ -72,14 +72,54 @@ public partial class admin_editcourse : System.Web.UI.Page
                     }
                     if (existingDates.Count > 0)
                     {
+                        var existingdateInApplicationMaster = (from am in db.applicationmaster
+                                                               join coursedate in db.course_dates on am.course equals coursedate.courseid into data
+                                                               from x1 in data.DefaultIfEmpty()
+                                                               where x1.id.ToString() == am.commencementdate && am.course == courseID
+                                                               select x1).ToList();
+
                         for (int i = 0; i < existingDates.Count; i++)
+                        {
                             hidCommencementDates.Value += existingDates[i].commencementdate.ToString("dd-MM-yyyy") + ",";
+                            var date = existingDates[i].commencementdate;
+
+                            int cnt = existingdateInApplicationMaster.Where(c => c.commencementdate == date).ToList().Count;
+                            if (cnt > 0)
+                            {
+                                hidExistingCommencementDates.Value += date.ToString("dd-MM-yyyy") + ",";
+                            }
                         }
+                        hidExistingCommencementDateIds.Value = "";
+                        for (int i = 0; i < existingdateInApplicationMaster.Count; i++)
+                        {
+                            hidExistingCommencementDateIds.Value += (hidExistingCommencementDateIds.Value == "") ? existingdateInApplicationMaster[i].id.ToString() : "," + existingdateInApplicationMaster[i].id.ToString();
+                        }
+                    }
 
                     if (exsistingderementDate != null && exsistingderementDate.Count > 0)
                     {
+                        var existingDefermentdate = (from am in db.applicationmaster
+                                                     join coursedate in db.course_defermentdates on am.course equals coursedate.courseid into data
+                                                     from x1 in data.DefaultIfEmpty()
+                                                     where x1.defermentdate == am.deferment_date && am.course == courseID
+                                                     select x1).ToList();
                         for (int i = 0; i < exsistingderementDate.Count; i++)
+                        {
                             Hiddefermentdates.Value += exsistingderementDate[i].defermentdate.ToString("dd-MM-yyyy") + ",";
+                            var derementdate = exsistingderementDate[i].defermentdate;
+                            int existingDefermentdateCount = existingDefermentdate.Where(d => d.defermentdate == derementdate).ToList().Count;
+
+                            if (existingDefermentdateCount > 0)
+                            {   
+                                 hidExixtingDefermentDate.Value += derementdate.ToString("dd-MM-yyyy") + ",";
+                            }
+                        }
+                        hidExistingDefermentDateIds.Value = "";
+                        for (int i = 0; i < existingDefermentdate.Count; i++)
+                        {
+                            hidExistingDefermentDateIds.Value += (hidExistingDefermentDateIds.Value == "") ? existingDefermentdate[i].id.ToString() : "," + existingDefermentdate[i].id.ToString();
+                        }
+
                     }
                        
                     if (mappings != null)
@@ -214,11 +254,19 @@ public partial class admin_editcourse : System.Web.UI.Page
             db.course_campus_mapping.RemoveRange(existingMappings);
             db.SaveChanges();
         }
-
-        var existingCourseDates = db.course_dates.Where(x => x.courseid == CourseID).ToList();
+        var hidExistingCommencementDateIdArr = hidExistingCommencementDateIds.Value.Split(Convert.ToChar(","));
+        var existingCourseDates = db.course_dates.Where(x => x.courseid == CourseID && !hidExistingCommencementDateIdArr.Contains(x.id.ToString())).ToList();
         if (existingCourseDates != null)
         {
             db.course_dates.RemoveRange(existingCourseDates);
+            db.SaveChanges();
+        }
+
+        var hidExistingdefermentdatesIdArr = hidExistingDefermentDateIds.Value.Split(Convert.ToChar(","));
+        var existingDefermentDates = db.course_defermentdates.Where(x => x.courseid == CourseID && !hidExistingdefermentdatesIdArr.Contains(x.id.ToString())).ToList();
+        if (existingDefermentDates != null)
+        {
+            db.course_defermentdates.RemoveRange(existingDefermentDates);
             db.SaveChanges();
         }
 
@@ -234,19 +282,36 @@ public partial class admin_editcourse : System.Web.UI.Page
             CourseObj.universityid = Convert.ToInt32(ddlUniversity.SelectedValue);
             db.SaveChanges();
 
+            //commencement date
             var commencementDates = hidCommencementDates.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var item in commencementDates)
+            var existingCommencementDates = hidExistingCommencementDates.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> newDates = new List<string>();
+            for (var i = 0; i < commencementDates.Length; i++)
+            {
+                var commencementDateCount = existingCommencementDates.Where(c => c == commencementDates[i]).ToList().Count;
+                if (commencementDateCount == 0)
+                    newDates.Add(commencementDates[i]);
+            }
+
+            foreach (var item in newDates)
             {
                 var dateArr = item.Split(Convert.ToChar("-"));
                 var date = Convert.ToDateTime(string.Concat(dateArr[2], "-", dateArr[1], "-", dateArr[0]));
-                course_dates course_date = new course_dates() { courseid = CourseObj.courseid, commencementdate = date };
-                db.course_dates.Add(course_date);
+
+                course_dates course_dates = new course_dates() { courseid = CourseObj.courseid, commencementdate = date };
+                db.course_dates.Add(course_dates);
             }
-
-               
-
+            //deferment Date 
             var defermentdates = Hiddefermentdates.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var item in defermentdates)
+            var existingdefermentdates = hidExixtingDefermentDate.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> newdefermentsdate = new List<string>();
+            for (var i = 0; i < defermentdates.Length; i++)
+            {
+                var defermentdatesCount = existingdefermentdates.Where(c => c == defermentdates[i]).ToList().Count;
+                if (defermentdatesCount == 0)
+                    newdefermentsdate.Add(defermentdates[i]);
+            }
+            foreach (var item in newdefermentsdate)
             {
                 var dateArr = item.Split(Convert.ToChar("-"));
                 var Defermentdate = Convert.ToDateTime(string.Concat(dateArr[2], "-", dateArr[1], "-", dateArr[0]));
