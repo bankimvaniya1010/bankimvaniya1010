53b7258903e14dc92874f1c75a6348bb133c7e7d
Akash <akash@sodelsolutions.com>
2019-11-28 18:27:43 +0530

    changes 1) Added logout page

---
+++ Applicationv.01/logout.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+
+public partial class logout : System.Web.UI.Page
+{
+    Logger objLog = new Logger();
+    string webURL = String.Empty;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        webURL = Utility.GetWebUrl();
+        Session["universityId"] = null;
+        Session["universityName"] = null;
+
+        Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.Cache.SetNoStore();
+
+        try
+        {
+            Session.Clear();
+            Session.Abandon();
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Buffer = true;
+            Response.ExpiresAbsolute = DateTime.Now.AddDays(-1d);
+            Response.Expires = -1000;
+            Response.CacheControl = "no-cache";
+        }
+        catch (Exception ex) { objLog.WriteLog(ex.ToString()); }
+
+        Response.Redirect(webURL + "login.aspx");
+    }
+}
