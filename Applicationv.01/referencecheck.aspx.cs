465912e15f39cb7029efe56dac6ac1931f30ed2b
Aishwarya <aishwarya@sodelsolutions.com>
2020-02-07 17:29:27 +0530

    change in email template of page referencecheck,verifyemployment,verifyeducationdetails

--- Applicationv.01/referencecheck.aspx.cs
+++ Applicationv.01/referencecheck.aspx.cs
@@ -12,35 +12,40 @@ public partial class referencecheck : System.Web.UI.Page
     Common objCom = new Common();
     Logger objLog = new Logger();
     protected List<tooltipmaster> lstToolTips = new List<tooltipmaster>();
-    string webURL = String.Empty;//System.Configuration.ConfigurationManager.AppSettings["WebUrl"].ToString();
+    string webURL = String.Empty;
+    public string lblfirstname = string.Empty;
     protected void Page_Load(object sender, EventArgs e)
     {
         webURL = Utility.GetWebUrl();
-        if (!Utility.CheckStudentLogin())
-            Response.Redirect(webURL + "Login.aspx", true);
         if ((Request.QueryString["referncekey"] != null) && (Request.QueryString["referncekey"] != ""))
             referenceKey = Convert.ToString(Request.QueryString["referncekey"]);
         else
             Response.Redirect(webURL + "error.aspx", true);
 
-        var RefInfo = (from pInfo in db.applicantreferencecheck
-                       where pInfo.referncekey == referenceKey
-                       select pInfo).FirstOrDefault();
-        if (RefInfo == null)
-            Response.Redirect(webURL + "error.aspx", true);
-        else
-        {
-            if (RefInfo.isverified == 1)
-            {
-                message.Visible = true;
-                button.Visible = false;
-                confirmation.Visible = false;
-            }
+        if (!IsPostBack) {
+                        
+            var RefInfo = (from pInfo in db.applicantreferencecheck
+                           where pInfo.referncekey == referenceKey
+                           select pInfo).FirstOrDefault();
+
+            if (RefInfo == null)
+                Response.Redirect(webURL + "error.aspx", true);
             else
             {
-                confirmation.Visible = true;
-                button.Visible = true;
+                if (RefInfo.isverified == 1)
+                {
+                    message.Visible = true;
+                    button.Visible = false;
+                    confirmation.Visible = false;
+                }
+                else
+                {
+                    confirmation.Visible = true;
+                    button.Visible = true;
+                }
             }
+            var applicantdetail = db.applicantdetails.Where(x => x.applicantid == RefInfo.applicantid && x.universityid == RefInfo.universityid).Select(x => new { x.firstname }).FirstOrDefault();
+            lblfirstname = applicantdetail.firstname;
         }
     }
     
@@ -48,16 +53,27 @@ public partial class referencecheck : System.Web.UI.Page
     {
         try
         {
+            var mode = "new";
             var RefInfo = (from pInfo in db.applicantreferencecheck
                            where pInfo.referncekey == referenceKey
                            select pInfo).FirstOrDefault();
-            if (chkVerify.Checked)
-                RefInfo.isverified = 1;
-            else
-                RefInfo.isverified = 1;
-            RefInfo.referenceverifiedtime = DateTime.Now;
+            applicantreferencecheck objapplicantreferencecheck = new applicantreferencecheck();
+            if (RefInfo != null)
+            {
+                mode = "update";
+                objapplicantreferencecheck = RefInfo;
+            }
+
+            objapplicantreferencecheck.howlongyouknow_refereesponse = txtquestion1.Value;
+            objapplicantreferencecheck.describeskill_refereesponse = txtquestion2.Value;
+            objapplicantreferencecheck.greateststrength_refereesponse = txtquestion3.Value;
+            objapplicantreferencecheck.whyyouchoosetoreferthisstudent_refereesponse = txtquestion4.Value;
+            objapplicantreferencecheck.isverified = 1;
+            objapplicantreferencecheck.referenceverifiedtime = DateTime.Now;
+            if (mode == "new")
+                db.applicantreferencecheck.Add(objapplicantreferencecheck);
             db.SaveChanges();
-            lblMessages.Visible = true;           
+            lblMessages.Visible = true;
             button.Visible = false;
             confirmation.Visible = false;
         }
--- Applicationv.01/verifyemployment.aspx.cs
+++ Applicationv.01/verifyemployment.aspx.cs
@@ -8,30 +8,55 @@ using System.Web.UI.WebControls;
 public partial class verifyemployment : System.Web.UI.Page
 {
     private GTEEntities db = new GTEEntities();
-    string webURL = String.Empty;//System.Configuration.ConfigurationManager.AppSettings["WebUrl"].ToString();
+    string webURL = String.Empty;
+    public string lblfirstname = string.Empty;
+    Logger objLog = new Logger();
+    Common objCom = new Common();
+    string verificationKey = string.Empty;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         webURL = Utility.GetWebUrl();
-        if (!Utility.CheckStudentLogin())
-            Response.Redirect(webURL + "Login.aspx", true);
+        verificationKey = Request.QueryString["key"];
         if (!IsPostBack)
         {
             if (Request.QueryString["key"] != null)
-            {
-                string verificationKey = Request.QueryString["key"];
+            {   
                 applicantemployerdetails details = db.applicantemployerdetails.Where(obj => obj.employerverificationkey == verificationKey).FirstOrDefault();
 
                 if (details != null)
                 {
+                    //info
+
+                    var applicantdetails = db.applicantdetails.Where(x => x.applicantid == details.applicantid && x.universityid == details.universityid).Select(x => new { x.firstname, x.totalyearofexperience }).FirstOrDefault();
+                    if (applicantdetails.firstname != null && applicantdetails.totalyearofexperience != null)
+                    {
+
+                        lblfirstname = applicantdetails.firstname;
+                        lblyearsofworkexper.InnerText = applicantdetails.totalyearofexperience;
+                    }
+                    lblnameoforganisation.InnerText = details.organization;
+                    lblreportingmanager.InnerText = details.nameofreportingmanger;
+                    lblposition.InnerText = details.designation;
+                    lbllocation.InnerText = objCom.GetCountryDiscription(Convert.ToInt32(details.country));
+                    if(details.durationfrom != null)
+                        lblstartdate.InnerText = Convert.ToDateTime(details.durationfrom).ToString("dd/MM/yyyy");
+                    if (details.durationto != null)
+                        lblEndDate.InnerText = Convert.ToDateTime(details.durationto).ToString("dd/MM/yyyy");
+                    else
+                    {
+                        if(details.iscurrentworking != null)
+                            lblEndDate.InnerText = "Currently Working";
+                    }
+                    lbljobdescription.InnerText = details.briefdescription;
+
                     if (details.isemployerdetailverified.HasValue && details.isemployerdetailverified.Value)
                     {
                         alreadyVerified();
                     }
                     else
                     {
-                        details.isemployerdetailverified = true;
-                        db.SaveChanges();
-                        afterVerification();
+                        questionDIv.Visible = true;
                     }
                 }
                 else
@@ -47,6 +72,7 @@ public partial class verifyemployment : System.Web.UI.Page
         detailsNotFound.Visible = false;
         verified.Visible = false;
         afterVerfication.Visible = true;
+        questionDIv.Visible = false;
     }
 
     private void alreadyVerified()
@@ -54,6 +80,7 @@ public partial class verifyemployment : System.Web.UI.Page
         detailsNotFound.Visible = false;
         afterVerfication.Visible = false;
         verified.Visible = true;
+        questionDIv.Visible = false;
     }
 
     private void notFound()
@@ -61,5 +88,38 @@ public partial class verifyemployment : System.Web.UI.Page
         detailsNotFound.Visible = true;
         afterVerfication.Visible = false;
         verified.Visible = false;
+        questionDIv.Visible = false;
+    }
+       
+    protected void btn_Save_Click1(object sender, EventArgs e)
+    {
+        try
+        {
+            var mode = "new";
+            var details = db.applicantemployerdetails.Where(obj => obj.employerverificationkey == verificationKey).FirstOrDefault();
+            applicantemployerdetails objapplicantemployerdetails = new applicantemployerdetails();
+            if (details != null)
+            {
+                mode = "update";
+                objapplicantemployerdetails = details;
+            }             
+            if (details != null)
+            {
+                objapplicantemployerdetails.isemployerdetailverified = true;
+                objapplicantemployerdetails.verifyrecord_refereeresponse = txtQuestion1.Value;
+                objapplicantemployerdetails.howlongyouknow_refereeresponse = txtQuestion2.Value;
+                objapplicantemployerdetails.opinionforstudycourse_refereeresponse = txtQuestion3.Value;
+                objapplicantemployerdetails.biggeststrenthknown_refereeresponse = txtQuestion4.Value;
+                objapplicantemployerdetails.getalongwithmanagement_refereeresponse = txtQuestion5.Value;
+                objapplicantemployerdetails.wouldyourehire_refereeresponse = txtQuestion6.Value;
+                if (mode == "new")
+                    db.applicantemployerdetails.Add(objapplicantemployerdetails);
+                db.SaveChanges();
+                afterVerification();
+            }
+            else
+                notFound();
+        }
+        catch (Exception ex) { objLog.WriteLog(ex.ToString()); }
     }
 }
