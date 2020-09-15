71c7017a08014fab7ed182177bd04391c150e04c
Aishwarya <aishwarya@sodelsolutions.com>
2020-09-15 11:04:36 +0530

    exam assign & exam schedule page changes

--- Applicationv.01/admin/exam_assignList.aspx.cs
+++ Applicationv.01/admin/exam_assignList.aspx.cs
@@ -23,14 +23,49 @@ public partial class admin_exam_assignList : System.Web.UI.Page
             Response.Redirect(webURL + "admin/Login.aspx", true);
 
         if (!IsPostBack)
-            bindUniversity();
+            BindUniversity();
     }
-    private void bindUniversity()
+    
+    private void BindGrid(int universityid, int selectedexamassignerid)
+    {
+        try
+        {
+            var examlList = (from es in db.exam_assign
+                             join ad in db.applicantdetails on es.applicantid equals ad.applicantid
+
+                             join em in db.exam_master on es.exampapersid equals em.exampapersid
+
+                             join eshe in db.exam_schedule on es.exampapersid equals eshe.exampapersid 
+
+                             join um in db.university_master on es.universityID equals um.universityid 
+
+                             where es.universityID == universityid && ad.universityid == universityid && es.exam_datetime == eshe.exam_datetime && es.examassignerid == selectedexamassignerid
+                             select new
+                             {
+                                 assignid = es.assignid,
+                                 universityname = um.university_name,
+                                 paper_name = em.exam_name,
+                                 studentname = es.applicantid +" "+ ad.firstname,
+                                 exam_datetime = eshe.exam_datetime,
+                             }).OrderBy(x => x.assignid).ToList();
+            if (examlList != null)
+            {
+                QuestiontGridView.DataSource = examlList;
+                QuestiontGridView.DataBind();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            objLog.WriteLog(ex.ToString());
+        }
+    }
+
+    private void BindUniversity()
     {
         try
         {
             ListItem lst = new ListItem("Please select", "0");
-            // List<formmaster> forms = new List<formmaster>();
             dynamic Universities;
             if (roleName.ToLower() == "admin")
                 Universities = db.university_master.ToList();
@@ -51,43 +86,73 @@ public partial class admin_exam_assignList : System.Web.UI.Page
             objLog.WriteLog(ex.ToString());
         }
     }
+
     protected void ddlUniversity_SelectedIndexChanged(object sender, EventArgs e)
     {
-        if (ddlUniversity.SelectedValue != "")
-            BindGrid(Convert.ToInt32(ddlUniversity.SelectedValue));
+        int selecteduniversityid = Convert.ToInt32(ddlUniversity.SelectedValue);
+        BindExaminer(selecteduniversityid);
     }
-    private void BindGrid(int universityid)
+
+    private void BindExaminer(int universityId)
     {
         try
         {
-            var examlList = (from es in db.exam_assign
-                             join ad in db.applicantdetails on es.applicantid equals ad.applicantid
-
-                             join em in db.exam_master on es.exampapersid equals em.exampapersid
-
-                             join eshe in db.exam_schedule on es.exampapersid equals eshe.exampapersid 
-
-                             join um in db.university_master on es.universityID equals um.universityid 
-
-                             where es.universityID == universityid && ad.universityid == universityid && es.exam_datetime == eshe.exam_datetime
-                             select new
-                             {
-                                 assignid = es.assignid,
-                                 universityname = um.university_name,
-                                 paper_name = em.exam_name,
-                                 studentname = es.applicantid +" "+ ad.firstname,
-                                 exam_datetime = eshe.exam_datetime,
-                             }).OrderBy(x => x.assignid).ToList();
-            if (examlList != null)
-            {
-                QuestiontGridView.DataSource = examlList;
-                QuestiontGridView.DataBind();
-            }
-
+            ListItem lst = new ListItem("Please select", "0");
+            var examinerId = db.examiner_master.Where(x => x.universityId == universityId && x.roleid == 12).ToList();
+            ddlexaminer.DataSource = examinerId;
+            ddlexaminer.DataTextField = "name";
+            ddlexaminer.DataValueField = "examinerID";
+            ddlexaminer.DataBind();
+            ddlexaminer.Items.Insert(0, lst);
         }
         catch (Exception ex)
         {
             objLog.WriteLog(ex.ToString());
         }
+    }
+
+    protected void ddlexaminer_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        creatediv.Attributes.Add("style", "display:none");
+        txtpasskey.Value = "";
+    }
+
+    protected void btn_submit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int selecteduniversityid = Convert.ToInt32(ddlUniversity.SelectedValue);
+            int selectedexaminerId = Convert.ToInt32(ddlexaminer.SelectedValue);
+            string enteredPasskey = txtpasskey.Value.Trim();
+            var examinerpasskey = db.examiner_master.Where(x => x.universityId == selecteduniversityid && x.examinerID == selectedexaminerId).Select(x => x.examinerkey).FirstOrDefault();
+
+            if (enteredPasskey == examinerpasskey)
+            {
+                validateDiv.Attributes.Add("style", "display:none");
+                creatediv.Attributes.Add("style", "display:block");
+                BindGrid(selecteduniversityid, selectedexaminerId);
+            }
+            else
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Entered passkey doest not match with records.')", true);
+        }
+        catch (Exception ex)
+        {
+            objLog.WriteLog(ex.ToString());
+        }
+    }
+
+    protected void btn_addnew_Click(object sender, EventArgs e)
+    {
+        int selecteduniversityid = Convert.ToInt32(ddlUniversity.SelectedValue);
+        int selectedexaminerId = Convert.ToInt32(ddlexaminer.SelectedValue);
+        Response.Redirect(webURL + "admin/exam_assign.aspx?universityid=" + selecteduniversityid + "&examinerid=" + selectedexaminerId + "", true);
+    }
+       
+    protected void QuestiontGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        int selecteduniversityid = Convert.ToInt32(ddlUniversity.SelectedValue);
+        int selectedexaminerId = Convert.ToInt32(ddlexaminer.SelectedValue);
+        QuestiontGridView.PageIndex = e.NewPageIndex;
+        BindGrid(selecteduniversityid, selectedexaminerId);
     }
 }
